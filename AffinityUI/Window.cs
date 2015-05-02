1392fee300e56ae3a03713a4332c28947d8dfad8
Matt Olenik <olenikm@gmail.com>
2015-05-01 17:42:39 -0700

    Window control, example KSP project

---
+++ AffinityUI/Window.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+namespace AffinityUI
+{
+    public class Window : Control
+    {
+        Control _content;
+        static readonly System.Random _rand;
+        readonly int _id;
+        Rect _dimensions;
+        string _text;
+
+        public int ID { get { return _id; }}
+
+        public Window()
+        {
+            _id = _rand.Next();
+        }
+
+        public static Window Create<TGuiTarget>(Rect dimensions, string text)
+        {
+            var result = new Window{ _dimensions = dimensions, _text = text };
+            result.TargetType = typeof(TGuiTarget);
+            if (result.TargetType != typeof(GUILayout) &&
+                result.TargetType != typeof(GUI))
+            {
+                throw new ArgumentException("Generic argument must be type GUI or GUILayout ", "TGuiTarget");
+            }
+            return result;
+        }
+
+        public Window SetContent(Control content)
+        {
+            _content = content;
+            return this;
+        }
+
+        protected override void Layout_GUI()
+        {
+        }
+
+        protected override void Layout_GUILayout()
+        {
+            GUILayout.Window(_id, _dimensions, windowFunc, _text);
+        }
+
+        void windowFunc(int windowID)
+        {
+            _content.Layout();
+        }
+    }
+}
---
+++ KspExample/ExampleGUI.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using AffinityUI;
+
+namespace KspExample
+{
+    [KSPAddon(KSPAddon.Startup.EveryScene, false)]
+    public class Starter : MonoBehaviour
+    {
+        protected void Start()
+        {
+            gameObject.AddComponent<ExampleGUI>();
+        }
+    }
+
+    public class ExampleGUI : MonoBehaviour
+    {
+        Control gui;
+
+        bool Option1 { get; set; }
+
+        Texture2D buttonIcon = GameDatabase.Instance.GetTexture("AffinityUIExample/icon", false);
+
+        void OnGUI()
+        {
+            if(gui == null)
+            {
+                //var gui = Window.Create<GUILayout>(new Rect(100, 100, 400, 400), "window test")
+                //.SetContent(
+                gui = Composite.Create<GUILayout, Area>()
+                    .SetDimensions(new Rect(20, 20, 200, 200))
+                    .Add(new Button("A Button")
+                        // This will print "A Button was clicked" on each click
+                        .OnClicked(s => Debug.Log(s.Label + " was clicked"))
+                    )
+                    .Add(new Toggle("Checkbox 1")
+                        // Bind the value of the checkbox to the Option1 variable
+                        .IsCheckedProperty.BindTwoWay(() => Option1, v => Option1 = v)
+                        // Binding to the visible property makes this control only visible
+                        // when Option1 is true, letting us show/hide it using the Toggle control below
+                        .VisibleProperty.BindOneWay(() => Option1)
+                    )
+                    .Add(new Toggle("Checkbox 2")
+                        // Bind the value of the checkbox to the Option1 variable
+                        .IsCheckedProperty.BindTwoWay(() => Option1, v => Option1 = v)
+                        // Print to the console each time the value changes
+                        .OnToggled((source, old, @new) => Debug.Log(source.Label + " is now " + @new))
+                    )
+                    .Add(new Box().SetLabel("sadf"));//);
+            }
+            gui.Layout();
+        }
+    }
+}
